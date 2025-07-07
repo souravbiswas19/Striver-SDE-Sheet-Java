92493e3dca0b634d5b246d854b4ba5ee463e27d6
souravbiswas19 <souravbiswas19april@gmail.com>
2025-07-07 22:10:14 +0530

    D-1

--- Day-1/Maximum_Subarray_Sum.java
+++ Day-1/Maximum_Subarray_Sum.java
@@ -1,13 +1,4 @@
 public class Maximum_Subarray_Sum {
-    public static void main(String[] args) {
-        // Sample input array
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-
-        // Call the maxSubArray method and print the result
-        int result = maxSubArray(nums);
-        System.out.println("Maximum Subarray Sum is: " + result);
-    }
-
     public static int maxSubArray(int[] nums) {
         int sum = 0, maxi = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
@@ -21,4 +12,14 @@ public class Maximum_Subarray_Sum {
         }
         return maxi;
     }
+
+    public static void main(String[] args) {
+        // Sample input array
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        // Call the maxSubArray method and print the result
+        int result = maxSubArray(nums);
+        System.out.println("Maximum Subarray Sum is: " + result);
+    }
+
 }
